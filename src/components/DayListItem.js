67d58cfb6ea23295b39b6fb6ceefec806caeaf69
CJM1994 <connorjohnmullin@gmail.com>
2022-03-22 18:10:33 -0700

    Refactor and add format spots function to DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,15 +4,27 @@ import 'components/DayListItem.scss';
 
 export default function DayListItem(props) {
 
+  const { setDay, name, spots, selected } = props;
+
   const dayClass = classNames('day-list__item', {
-    'day-list__item--selected': props.selected,
-    'day-list__item--full': props.spots === 0
+    'day-list__item--selected': selected,
+    'day-list__item--full': spots === 0
   });
 
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return 'no spots';
+    } else if (spots === 1) {
+      return '1 spot';
+    } else {
+      return `${spots} spots`
+    };
+  };
+
   return (
-    <li className={dayClass} onClick={() => { props.setDay(props.name) }}>
-      <h2 className='text--regular'>{props.name}</h2>
-      <h3 className='text--light'>{props.spots} spots remaining</h3>
+    <li className={dayClass} onClick={() => { setDay(name) }}>
+      <h2 className='text--regular'>{name}</h2>
+      <h3 className='text--light'>{formatSpots(spots)} remaining</h3>
     </li>
   );
 
