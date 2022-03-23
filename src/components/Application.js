a324e3522362049a40c1d05548d938c7573cb42d
CJM1994 <connorjohnmullin@gmail.com>
2022-03-22 20:31:15 -0700

    Add mock data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,26 @@
 import React from "react";
+import DayList from "./DayList";
 
 import "components/Application.scss";
+
+// Mock Data for testing purposes
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
 export default function Application(props) {
   return (
@@ -12,7 +32,9 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList days={days} day={'Monday'} setDay={day => console.log(day)} />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
