0502210f4f375bb7922bdc66ead8cf03b60db830
CJM1994 <connorjohnmullin@gmail.com>
2022-03-23 13:34:36 -0700

    refactor interviewlist component to not pass id prop as it is not needed

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -7,7 +7,7 @@ export default function InterviewerListItem(props) {
   const interviewerClass = classNames('interviewers__item', { 'interviewers__item--selected': props.selected });
 
   return (
-    <li onClick={() => { props.setInterviewer(props.id) }} className={interviewerClass}>
+    <li onClick={props.setInterviewer} className={interviewerClass}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
