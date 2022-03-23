fae98ef3af5c9d442e4cf031fac39b7889b5fe0a
CJM1994 <connorjohnmullin@gmail.com>
2022-03-23 11:11:45 -0700

    Add event handler and conditional props to InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import 'components/InterviewerListItem.scss'
+import classNames from 'classnames';
 
 export default function InterviewerListItem(props) {
 
+  const interviewerClass = classNames('interviewers__item', { 'interviewers__item--selected': props.selected });
+
   return (
-    <li className="interviewers__item">
+    <li onClick={() => { props.setInterviewer(props.id) }} className={interviewerClass}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected && props.name}
     </li>
   );
 
