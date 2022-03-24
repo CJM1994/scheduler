037e8093a2458f4d41f8389176b5a2908e92a4a1
CJM1994 <connorjohnmullin@gmail.com>
2022-03-23 17:48:27 -0700

    Add components Show/Status/Confirm and update stories

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,9 @@ import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
 
 storiesOf("Button", module)
   .addParameters({
@@ -144,3 +147,19 @@ storiesOf('Appointment', module)
   .add('Appointment with Time', () => <Appointment time='12pm' />)
   .add('Header', () => <Header time='12pm' />)
   .add('Empty', () => <Empty onAdd={action('onAdd')} />)
+  .add('Show', () => (
+    <Show
+      student='Lydia Miller-Jones'
+      interviewer={interviewer}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
+  ))
+  .add('Confirm', () => (
+    <Confirm
+      message='Delete the appointment?'
+      onCancel={action('onCancel')}
+      onConfirm={action('onConfirm')}
+    />
+  ))
+  .add('Status', () => <Status message='Deleting' />)
