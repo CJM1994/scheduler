443882c02ee40b3b38901a9d4318103e9ad9c3d0
CJM1994 <connorjohnmullin@gmail.com>
2022-03-25 17:34:38 -0700

    Add custom hook useVisualMode to conditionally render components

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = function (newMode, replace = false) {
+
+    if (replace) {
+      setHistory(prev => {
+        prev.pop();
+        return [...prev, newMode];
+      });
+      setMode(newMode);
+    } else {
+      setHistory([...history, newMode]);
+      return setMode(newMode);
+    };
+
+  };
+
+  const back = function () {
+    setHistory((prev) => {
+      if (prev.length > 1) {
+        prev.pop();
+        setMode(prev[prev.length - 1]);
+      };
+      return prev;
+    });
+  };
+
+  return { mode, transition, back };
+};
